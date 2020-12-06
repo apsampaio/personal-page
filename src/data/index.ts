0f2de6e4f4f77991438e8e076a94bc8f2db05354
apsampaio <andre03.sampaio@gmail.com>
2020-12-06 13:13:26 -0300

    add card data

---
+++ src/data/index.ts
@@ -0,0 +1,25 @@
+import {CardProps} from '../components/Card';
+
+import javascript from "../images/icons/javascript.png";
+import html from "../images/icons/html.png";
+import css from "../images/icons/css.png";
+
+const data: CardProps[] = [
+    {
+        title: "JavaScript",
+        color: "#F7DF1E",
+        path: javascript
+    },
+    {
+        title: "HTML5",
+        color: "#FF6D00",
+        path: html
+    },
+    {
+        title: "CSS",
+        color: "#29A9DF",
+        path: css
+    }
+];
+
+export default data;
